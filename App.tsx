24882d672673579862b9d1ce2627f557ee45d146
gowtham <gowtham.balu@bounteous.com>
2023-05-22 11:32:54 +0530

    updated icon changes

--- App.tsx
+++ App.tsx
@@ -12,26 +12,27 @@ export default function App() {
       <Button
         type={BUTTON_TYPES.PRIMARY}
         text="Hi Durga "
-        // icon={
-        //   <svg
-        //     xmlns="http://www.w3.org/2000/svg"
-        //     class="icon icon-tabler icon-tabler-arrow-right"
-        //     width="20"
-        //     height="20"
-        //     viewBox="0 0 24 24"
-        //     stroke-width="1.5"
-        //     stroke="#ffffff"
-        //     fill="none"
-        //     stroke-linecap="round"
-        //     stroke-linejoin="round"
-        //   >
-        //     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-        //     <path d="M5 12l14 0" />
-        //     <path d="M13 18l6 -6" />
-        //     <path d="M13 6l6 6" />
-        //   </svg>
-        // }
-        // iconposition='right'
+        //icon only allowed for web please comment it when running for mobile apps, will try to fix it in future 
+        icon={
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="icon icon-tabler icon-tabler-arrow-right"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="#ffffff"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M5 12l14 0" />
+            <path d="M13 18l6 -6" />
+            <path d="M13 6l6 6" />
+          </svg>
+        }
+        iconposition='right'
         
         onClick={onBtnClick}
         size="large"
