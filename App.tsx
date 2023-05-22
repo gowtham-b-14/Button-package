78f8226c6772f77780bb5d324e7436fcf83e4b57
gowtham <gowtham.balu@bounteous.com>
2023-05-22 11:17:37 +0530

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet,View,Text } from "react-native";
+import Button from "./components/Button";
+import { BUTTON_TYPES } from "./data/Constant";
 
 export default function App() {
+  const onBtnClick=()=>{
+    console.log("I'm clicked")
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button
+        type={BUTTON_TYPES.PRIMARY}
+        text="Hi Durga "
+        // icon={
+        //   <svg
+        //     xmlns="http://www.w3.org/2000/svg"
+        //     class="icon icon-tabler icon-tabler-arrow-right"
+        //     width="20"
+        //     height="20"
+        //     viewBox="0 0 24 24"
+        //     stroke-width="1.5"
+        //     stroke="#ffffff"
+        //     fill="none"
+        //     stroke-linecap="round"
+        //     stroke-linejoin="round"
+        //   >
+        //     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        //     <path d="M5 12l14 0" />
+        //     <path d="M13 18l6 -6" />
+        //     <path d="M13 6l6 6" />
+        //   </svg>
+        // }
+        // iconposition='right'
+        
+        onClick={onBtnClick}
+        size="large"
+        loading="true"
+      />
+      <Button type={BUTTON_TYPES.SECONDARY} text="How" onPress={onBtnClick}/>
+      <Button type={BUTTON_TYPES.TERTIARY} text="are you !"/>
+      <Button type={BUTTON_TYPES.DASHED} text="Welcome to our page... !"/>
+      <Button type={BUTTON_TYPES.DANGER} text="Don't Click here ... ☠️" loading="true" loadingcolor="red"/>
+      <Button type={BUTTON_TYPES.TEXT} text="Instead Click here ... ❤️"/>
     </View>
   );
 }
@@ -13,8 +49,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent:"space-evenly",
+    flexDirection:"row",
+    flexWrap:"wrap",
+    alignContent:"center"
   },
 });
---
+++ components/Button.tsx
@@ -0,0 +1,211 @@
+import React, { useState } from "react";
+import { BUTTON_TYPES } from "../data/Constant";
+//import "./button.css";
+import {
+  ActivityIndicator,
+  StyleSheet,
+  View,
+  Text,
+  Platform,
+  TouchableHighlight,
+  TouchableOpacity,
+} from "react-native";
+
+const Button = (props: any) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const {
+    type,
+    text,
+    icon,
+    iconposition,
+    onClick,
+    size,
+    loading,
+    loadingcolor,
+  } = props;
+  const isWeb = Platform.OS === "web";
+  const ButtonComponent: any = isWeb ? TouchableHighlight : TouchableOpacity;
+  let styling: Array<object> = [];
+  const getButtonClass = () => {
+    switch (type) {
+      case BUTTON_TYPES.PRIMARY:
+        return "primaryButton";
+      case BUTTON_TYPES.SECONDARY:
+        return "secondaryButton";
+      case BUTTON_TYPES.TERTIARY:
+        return "tertiaryButton";
+      case BUTTON_TYPES.DASHED:
+        return "dashedButton";
+      case BUTTON_TYPES.DANGER:
+        return "dangerButton";
+      case BUTTON_TYPES.TEXT:
+        return "textButton";
+      default:
+        return "secondaryBtn button";
+    }
+  };
+  const stylesforbutton = () => {
+    switch (type) {
+      case BUTTON_TYPES.PRIMARY:
+        return (styling = [
+          styles.primaryBtn,
+          styles.button,
+          isHovered && styles.primaryBtnhovered,
+        ]);
+      case BUTTON_TYPES.SECONDARY:
+        return (styling = [
+          styles.secondaryBtn,
+          styles.button,
+          isHovered && styles.secondaryBtnhovered,
+        ]);
+      case BUTTON_TYPES.TERTIARY:
+        return (styling = [
+          styles.tertiaryBtn,
+          styles.button,
+          isHovered && styles.tertiaryBtnhovered,
+        ]);
+      case BUTTON_TYPES.DASHED:
+        return (styling = [
+          styles.dashedBtn,
+          styles.button,
+          isHovered && styles.dashedBtnhovered,
+        ]);
+      case BUTTON_TYPES.DANGER:
+        return (styling = [
+          styles.dangerBtn,
+          styles.button,
+          isHovered && styles.dangerBtnhovered,
+        ]);
+      case BUTTON_TYPES.TEXT:
+        return (styling = [
+          styles.textBtn,
+          styles.button,
+          isHovered && styles.textBtnhovered,
+        ]);
+      default:
+        return (styling = [
+          styles.secondaryBtn,
+          styles.button,
+          isHovered && styles.secondaryBtnhovered,
+        ]);
+    }
+  };
+
+  return (
+    <ButtonComponent
+      style={stylesforbutton()}
+      className={getButtonClass()}
+      onPress={onClick ? onClick : () => console.log("Nothing is happening")}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <Text>
+        {isWeb
+          ? loading &&
+            loading === "true" && (
+              <ActivityIndicator
+                color={loadingcolor ? `${loadingcolor}` : "white"}
+                style={{ padding: 5 }}
+              />
+            )
+          : console.log("sorry In mobile you can not add login icon !")}
+        {icon && iconposition === "left" && icon}
+        <Text
+          style={
+            type === BUTTON_TYPES.DANGER ? { color: "red" } : { color: "black" }
+          }
+        >
+          {text}
+        </Text>
+
+        {icon && iconposition === "right" && icon}
+      </Text>
+    </ButtonComponent>
+  );
+};
+
+export default Button;
+
+const styles = StyleSheet.create({
+  button: {
+    padding: 10,
+    cursor: "pointer",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  primaryBtn: {
+    backgroundColor: "#1677ff",
+    padding: "12px 24px",
+    borderRadius: 6,
+    color: "white",
+    fontWeight: "600px",
+    letterSpacing: "1.2px",
+    transition: "background-color 0.3s ease",
+  },
+  dashedBtn: {
+    padding: "12px 24px",
+    borderRadius: 6,
+    color: "black",
+    fontWeight: "600px",
+    letterSpacing: "1.2px",
+    borderWidth: 1,
+    borderStyle: "dashed",
+    borderColor: "lightgrey",
+  },
+  textBtn: {
+    padding: "12px 24px",
+    color: "black",
+    fontWeight: "600px",
+    letterSpacing: "1.2px",
+  },
+  dangerBtn: {
+    padding: "12px 24px",
+    borderRadius: 6,
+    fontWeight: "600px",
+    letterSpacing: "1.2px",
+    borderWidth: 1,
+    borderStyle: "solid",
+    borderColor: "red",
+  },
+  secondaryBtn: {
+    padding: "12px 24px",
+    borderRadius: 6,
+    color: "black",
+    fontWeight: "600px",
+    letterSpacing: "1.2px",
+    borderWidth: 1,
+    borderStyle: "solid",
+    borderColor: "lightgrey",
+    transition: "background-color 0.3s ease",
+  },
+  tertiaryBtn: {
+    padding: "12px 24px",
+    color: " #1677ff",
+    fontWeight: "600px",
+    letterSpacing: "1.2px",
+  },
+  primaryBtnhovered: {
+    transform: [{ scale: 1.2 }],
+  },
+  secondaryBtnhovered: {
+    borderColor: "#1677ff",
+    transform: [{ scale: 1.2 }],
+  },
+  dashedBtnhovered: {
+    borderColor: "#1677ff",
+    transform: [{ scale: 1.2 }],
+  },
+  dangerBtnhovered: {
+    borderColor: "#ffa39e",
+    transform: [{ scale: 1.2 }],
+  },
+  textBtnhovered: {
+    backgroundColor: "lightgrey",
+    borderRadius: 6,
+    transform: [{ scale: 1.2 }],
+  },
+  tertiaryBtnhovered: {
+    transform: [{ scale: 1.2 }],
+  },
+});
